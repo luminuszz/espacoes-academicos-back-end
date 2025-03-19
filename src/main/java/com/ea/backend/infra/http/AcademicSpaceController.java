c420c0d2fea0463d88e03f89419b6775baa1da40
luminuszz <luminuszz43@gmail.com>
2025-03-19 16:22:08 -0300

    feat(domain-spaces): add AcademicSpaceController.java
    Signed-off-by: luminuszz <luminuszz43@gmail.com>

---
+++ src/main/java/com/ea/backend/infra/http/AcademicSpaceController.java
@@ -0,0 +1,9 @@
+package com.ea.backend.infra.http;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/spaces")
+public class AcademicSpaceController {
+}
