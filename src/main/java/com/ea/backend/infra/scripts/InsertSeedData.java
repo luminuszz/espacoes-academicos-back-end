d106c1c0c045c7f609a182f9c92fa1a00e751f8d
luminuszz <luminuszz43@gmail.com>
2025-04-19 09:45:21 -0300

    fix(domain-reservation): fix reservations
    Signed-off-by: luminuszz <luminuszz43@gmail.com>

--- src/main/java/com/ea/backend/infra/scripts/InsertSeedData.java
+++ src/main/java/com/ea/backend/infra/scripts/InsertSeedData.java
@@ -1,15 +1,14 @@
 package com.ea.backend.infra.scripts;
 
-import com.ea.backend.domain.reservation.enterprise.entity.ReservationStatus;
 import com.github.javafaker.Faker;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Random;
 import java.util.UUID;
 
@@ -36,6 +35,7 @@ public class InsertSeedData implements CommandLineRunner {
     System.out.println("Insert SeedData seed  " + USER);
     System.out.println("Insert SeedData seed  " + PASSWORD);
 
+    
     var random = new Random();
 
     if (!Boolean.parseBoolean(RUN_SEED)) {
@@ -47,37 +47,7 @@ public class InsertSeedData implements CommandLineRunner {
 
     try (Connection conn = DriverManager.getConnection(JDBC_URL, USER, PASSWORD)) {
 
-      conn.setAutoCommit(false);
 
-
-      // Insert 50 school units
-      String insertSchoolUnitSQL =
-              "INSERT INTO public.unidade_escola (id, name, type, state, city, address, numero_contato, data_criacao) " +
-                      "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-
-      List<UUID> schoolUnitIds = new ArrayList<>();
-      try (PreparedStatement stmt = conn.prepareStatement(insertSchoolUnitSQL)) {
-        for (int i = 1; i <= 50; i++) {
-          UUID schoolUnitId = UUID.randomUUID();
-          schoolUnitIds.add(schoolUnitId);
-          stmt.setObject(1, schoolUnitId);
-          stmt.setString(2, faker.educator().university());
-          stmt.setString(3, faker.options().option("Public", "Private"));
-          stmt.setString(4, faker.address().state());
-          stmt.setString(5, faker.address().city());
-          stmt.setString(6, faker.address().streetAddress());
-
-          var contactNumber = faker.phoneNumber().phoneNumber();
-          if (contactNumber.length() > 15) {
-            contactNumber = contactNumber.substring(0, 15);
-          }
-
-          stmt.setString(7, contactNumber);
-          stmt.setTimestamp(8, new Timestamp(System.currentTimeMillis())); // Current timestamp
-          stmt.addBatch();
-        }
-        stmt.executeBatch();
-      }
 
       // Insert 50 users
       String insertUserSQL =
@@ -105,101 +75,6 @@ public class InsertSeedData implements CommandLineRunner {
         throw new RuntimeException(e);
       }
 
-
-      List<UUID> userIds = new ArrayList<>();
-      try (PreparedStatement stmt = conn.prepareStatement(insertUserSQL)) {
-        for (int i = 1; i <= 50; i++) {
-          UUID userId = UUID.randomUUID();
-          UUID schoolUnitId = schoolUnitIds.get(random.nextInt(schoolUnitIds.size()));
-          // Alternating between ADMIN and TEACHER
-          var role = i % 2 == 0 ? "ADMIN" : "TEACHER";
-          userIds.add(userId);
-
-          stmt.setObject(1, userId);
-          stmt.setString(2, faker.internet().emailAddress());
-          stmt.setString(3, faker.name().fullName());
-          stmt.setString(4, encoder.encode(faker.internet().password())); // Fake password
-          stmt.setString(5, role);
-          stmt.setString(6, faker.phoneNumber().phoneNumber());
-
-
-          if (role.equals("TEACHER")) {
-            stmt.setString(7, faker.educator().course()); // Course
-            stmt.setObject(8, schoolUnitId);
-          } else {
-            stmt.setString(7, null); // No course for ADMIN;
-            stmt.setObject(8, null); // No school unit for ADMIN
-          }
-
-          stmt.addBatch();
-        }
-        stmt.executeBatch();
-      }
-
-      // Insert 50 spaces with random status
-      String insertSpaceSQL =
-          "INSERT INTO public.spaces (id, capacity, description, room_name, status, acronym) VALUES (?, ?, ?, ?, ?, ?)";
-      List<UUID> spaceIds = new ArrayList<>();
-      try (PreparedStatement stmt = conn.prepareStatement(insertSpaceSQL)) {
-        for (int i = 1; i <= 50; i++) {
-          UUID spaceId = UUID.randomUUID();
-          spaceIds.add(spaceId);
-          stmt.setObject(1, spaceId);
-          stmt.setInt(
-              2, faker.number().numberBetween(20, 120)); // Random capacity between 20 and 120
-          stmt.setString(3, faker.lorem().sentence());
-          stmt.setString(
-              4, "Room " + faker.letterify("??") + i); // Room name based on letters and number
-          stmt.setString(5, faker.bool().bool() ? "AVAILABLE" : "UNAVAILABLE"); // Random status
-          stmt.setString(6, "ACRONYM-" + i); // Acronym
-          stmt.addBatch();
-        }
-        stmt.executeBatch();
-      }
-
-      // Insert 50 reservations with random status and relationships
-      String insertReservationSQL =
-          "INSERT INTO public.reservations (id, end_date_time, start_date_time, academic_space_id, user_id, status) VALUES (?, ?, ?, ?, ?, ?)";
-      try (PreparedStatement stmt = conn.prepareStatement(insertReservationSQL)) {
-        for (int i = 1; i <= 50; i++) {
-          UUID reservationId = UUID.randomUUID();
-          UUID spaceId = spaceIds.get(random.nextInt(spaceIds.size()));
-          UUID userId = userIds.get(random.nextInt(userIds.size()));
-          Timestamp startDateTime =
-              new Timestamp(
-                  System.currentTimeMillis()
-                      + random.nextInt(
-                          7 * 24 * 60 * 60 * 1000)); // Random start time within the next 7 days
-          Timestamp endDateTime =
-              new Timestamp(
-                  startDateTime.getTime()
-                      + (random.nextInt(4) + 1)
-                          * 60
-                          * 60
-                          * 1000); // Random end time 1 to 4 hours after start time
-          String[] statuses = {
-            ReservationStatus.CONFIRMED_BY_THE_ENTERPRISE.toString(),
-            ReservationStatus.CONFIRMED_BY_THE_USER.toString(),
-                  ReservationStatus.SCHEDULED.toString(),
-            ReservationStatus.CANCELED.toString()
-          };
-          String status = statuses[random.nextInt(statuses.length)];
-
-          stmt.setObject(1, reservationId);
-          stmt.setTimestamp(2, endDateTime);
-          stmt.setTimestamp(3, startDateTime);
-          stmt.setObject(4, spaceId);
-          stmt.setObject(5, userId);
-          stmt.setString(6, status);
-          stmt.addBatch();
-        }
-        stmt.executeBatch();
-      }
-
-
-      // Commit the transaction
-      conn.commit();
-      System.out.println("Data inserted successfully!");
     } catch (SQLException e) {
       e.printStackTrace();
     }
